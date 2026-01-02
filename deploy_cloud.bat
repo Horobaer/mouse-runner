1b82acef91f621a21f877c7a57acc90730c8cfac
HHoroba <helge.horoba@alarmboard.de>
2026-01-02 20:45:25 +0100

    Add gcloud config set project to deploy script

--- deploy_cloud.bat
+++ deploy_cloud.bat
@@ -1,6 +1,13 @@
-@echo off
 echo Deploying Mouse Adventure to Google Cloud via Terraform...
 echo.
+
+echo Setting gcloud project...
+call gcloud config set project mouse-runner-481722
+if %errorlevel% neq 0 (
+    echo Error: Failed to set gcloud project.
+    pause
+    exit /b 1
+)
 
 echo Changing directory to terraform/cloud...
 cd terraform\cloud
